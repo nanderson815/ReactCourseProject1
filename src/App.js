9947fd2af97e9fae89fc61b521ac33d710fc2c61
Noah A <nanderson815@gmail.com>
2019-03-04 18:41:05 -0500

    adding new component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Hi, I am a react app!</h1>
-       <p>This is really working, my dood.</p>
-       <Person />
+        <h1>Hi, I am a react app!</h1>
+        <Person name="Noah" age="25" />
+        <Person name="Max" age="32">My hobbies: Racing</Person>
+        <Person name="Manu" age="28" />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const person = (props) => {
     return (
-        <p>I'm a Person!</p>
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old.</p>
+            <p>{props.children}</p>
+        </div>
     )
-}
+};
 
 export default person;
