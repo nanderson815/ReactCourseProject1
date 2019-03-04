c734a7d47fa2d8c84020441faccc949f4463aa62
Noah A <nanderson815@gmail.com>
2019-03-04 17:55:20 -0500

    adding new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <h1>Hi, I am a react app!</h1>
+       <p>This is really working, my dood.</p>
+       <Person />
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const person = (props) => {
+    return (
+        <p>I'm a Person!</p>
+    )
+}
+
+export default person;
