256a03e3f3db8e03f3c56ed32058783edb8e4794
Noah A <nanderson815@gmail.com>
2019-03-06 12:54:15 -0500

    added conditional formatting via scoped classes

--- src/App.js
+++ src/App.js
@@ -45,16 +45,8 @@ class App extends Component {
 
   render() {
 
-    const style = {
-      backgroundColor: "green",
-      color: "white",
-      font: 'inherit',
-      border: '1px solid green',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = null;
 
     if (this.state.showPersons) {
       persons = (
@@ -69,9 +61,7 @@ class App extends Component {
           })}
         </div>
       );
-
-      style.backgroundColor = 'red';
-      style.border = '1px solid red';
+      btnClass = Classes.Red;
     }
 
     const classes = [];
@@ -88,7 +78,7 @@ class App extends Component {
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
           <button
-            style={style}
+            className={btnClass}
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
