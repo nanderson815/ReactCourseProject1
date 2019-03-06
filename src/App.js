a8b2553b48f3359974bc47930705aadd26ae54bb
Noah A <nanderson815@gmail.com>
2019-03-06 12:19:39 -0500

    adding hover style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -51,7 +52,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid green',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -71,6 +76,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+          color: 'black'
+      };
       style.border = '1px solid red';
     }
 
@@ -96,4 +105,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
