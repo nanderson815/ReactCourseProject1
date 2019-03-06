210534349a9a30fde15d28def20b9d9f573200c0
Noah A <nanderson815@gmail.com>
2019-03-06 12:48:03 -0500

    adding css classes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import Classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -77,14 +77,14 @@ class App extends Component {
     const classes = [];
 
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      classes.push(Classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      classes.push(Classes.bold);
     }
 
     return (
-        <div className="App">
+        <div className={Classes.App}>
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
           <button
