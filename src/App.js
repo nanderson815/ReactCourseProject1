fdb619b8847ef90f32d89e5360e908ddb09279b4
Noah A <nanderson815@gmail.com>
2019-03-08 12:40:44 -0500

    adding errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundry/ErrorBoundry';
 
 class App extends Component {
   state = {
@@ -52,12 +53,11 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <ErrorBoundary key={person.id}><Person
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
+              changed={(event) => this.nameChangedHandler(event, person.id)} /></ErrorBoundary>
           })}
         </div>
       );
