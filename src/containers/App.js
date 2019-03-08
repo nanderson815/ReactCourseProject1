99e86c41fdb1fedd27feea0f0931cb638e0eceea
Noah A <nanderson815@gmail.com>
2019-03-08 17:32:05 -0500

    added console logs

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    console.log("[App.js] constructor");
+  }
+
   state = {
     persons: [
       { id: '1', name: 'Max', age: 28 },
@@ -12,6 +17,15 @@ class App extends Component {
     ],
     otherState: 'some other value'
   };
+
+  static getDerivedStateFromProps(props, state){
+    console.log('[App.js] gerDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount(){
+    console.log("[app.js] componentdidmount")
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -45,7 +59,7 @@ class App extends Component {
   }
 
   render() {
-
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
@@ -63,6 +77,7 @@ class App extends Component {
     return (
       <div className={Classes.App}>
         <Cockpit
+        title={this.props.appTitle}
         showPersons={this.state.showPersons}
         persons={this.state.persons}
         clicked={this.togglePersonsHandler}/>
